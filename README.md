2f5c339d9fb36ca1a713697391bf13956727f3e2
Kevin Hong <khong10@jhu.edu>
2019-08-22 01:19:17 -0500

    Added OCR text extraction

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ The idea for this project came from the fact that I wanted to summarize readings
 
 The script parses text from a user input PDF file and processes it by removing stop words and unnecessary characters. Every word is then normalized with a Porter2 stemmer and added to a word frequency table. The value of each sentence is calculated using the word frequencies, and only sentences that exceed the average sentence value by a certain factor are added into the summary.
 
+Update: Added the option to use OCR to extract the text from the PDF file. This is a slower process because the pages need to be individually converted into images, but the results are more accurate and now scanned documents and books can be summarized!
+
 ## Current Goals
 
-- Implement OCR techniques to extract text from PDF files that contain images of text rather than actual text (e.g. scanned documents and books)
 - Allow user to adjust degree of summarization using the Flesch-Kincaid readability test
 - Resolve undefined behavior with certain PDF file formatting
 
--- src/pdfSummarizer.py
+++ src/pdfSummarizer.py
@@ -2,79 +2,123 @@
 
 """pdfSummarizer.py: This script summarizes text provided in a PDF file."""
 
-import slate3k as slate
+import os
+import nltk
+import pytesseract
 import re
+import slate3k as slate
+from pdf2image import convert_from_path
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem.snowball import SnowballStemmer
+from PIL import Image
+nltk.download("stopwords")
+nltk.download("punkt")
+
+
+def extractText(file):
+    pdfFileObj = open(pdfFileName, "rb")
+    pdfPages = slate.PDF(pdfFileObj)
+
+    # Extract text from PDF file
+    text = ""
+    for page in pdfPages:
+        text += page
+    return text
+
+
+def extractOCR(file):
+    pages = convert_from_path(file, 500)
+
+    image_counter = 1
+    for page in pages:
+        filename = "page_" + str(image_counter) + ".jpg"
+        page.save(filename, "JPEG")
+        image_counter = image_counter + 1
+
+    limit = image_counter-1
+    text = ""
+    for i in range(1, limit + 1):
+        filename = "page_" + str(i) + ".jpg"
+        page = str(((pytesseract.image_to_string(Image.open(filename)))))
+        page = page.replace("-\n", "")
+        text += page
+        os.remove(filename)
+    return text
+
+
+def summarize(text):
+    # Process text by removing numbers and unrecognized punctuation
+    processedText = re.sub("’", "'", text)
+    processedText = re.sub("[^a-zA-Z' ]+", " ", processedText)
+    stopWords = set(stopwords.words("english"))
+    words = word_tokenize(processedText)
+
+    # Normalize words with Porter stemming and build word frequency table
+    stemmer = SnowballStemmer("english", ignore_stopwords=True)
+    freqTable = dict()
+    for word in words:
+        word = word.lower()
+        if word in stopWords:
+            continue
+        elif stemmer.stem(word) in freqTable:
+            freqTable[stemmer.stem(word)] += 1
+        else:
+            freqTable[stemmer.stem(word)] = 1
+
+    # Normalize every sentence in the text
+    sentences = sent_tokenize(text)
+    stemmedSentences = []
+    sentenceValue = dict()
+    for sentence in sentences:
+        stemmedSentence = []
+        for word in sentence.lower().split():
+            stemmedSentence.append(stemmer.stem(word))
+        stemmedSentences.append(stemmedSentence)
+
+    # Calculate value of every normalized sentence based on word frequency table
+    # [:12] helps to save space
+    for num in range(len(stemmedSentences)):
+        for wordValue in freqTable:
+            if wordValue in stemmedSentences[num]:
+                if sentences[num][:12] in sentenceValue:
+                    sentenceValue[sentences[num][:12]] += freqTable.get(wordValue)
+                else:
+                    sentenceValue[sentences[num][:12]] = freqTable.get(wordValue)
+
+    # Determine average value of a sentence in the text
+    sumValues = 0
+    for sentence in sentenceValue:
+        sumValues += sentenceValue.get(sentence)
+
+    average = int(sumValues / len(sentenceValue))
+
+    # Create summary of text using sentences that exceed the average value by some factor
+    # This factor can be adjusted to reduce/expand the length of the summary
+    summary = ""
+    for sentence in sentences:
+            if sentence[:12] in sentenceValue and sentenceValue[sentence[:12]] > (3.0 * average):
+                summary += " " + " ".join(sentence.split())
+
+    # Process the text in summary and write it to a new file
+    summary = re.sub("’", "'", summary)
+    summary = re.sub("[^a-zA-Z0-9'\"():;,.!?— ]+", " ", summary)
+    summaryText = open(fileName + "Summary.txt", "w")
+    summaryText.write(summary)
+    summaryText.close()
+
 
 # Scan user input for PDF file name
 print("What is the name of the PDF?")
 fileName = input("(Without .pdf file extension)\n")
 pdfFileName = fileName + ".pdf"
-pdfFileObj = open(pdfFileName, "rb")
-pdfPages = slate.PDF(pdfFileObj)
+option = input("Direct text extraction or OCR extraction? (text / OCR)\n")
 
-# Extract text from PDF file
-text = ""
-for page in pdfPages:
-    text += page
-
-# Process text by removing numbers and unrecognized punctuation
-processedText = re.sub("’", "'", text)
-processedText = re.sub("[^a-zA-Z' ]+", " ", processedText)
-stopWords = set(stopwords.words("english"))
-words = word_tokenize(processedText)
-
-# Normalize words with Porter stemming and build word frequency table
-stemmer = SnowballStemmer("english", ignore_stopwords=True)
-freqTable = dict()
-for word in words:
-    word = word.lower()
-    if word in stopWords:
-        continue
-    elif stemmer.stem(word) in freqTable:
-        freqTable[stemmer.stem(word)] += 1
-    else:
-        freqTable[stemmer.stem(word)] = 1
-
-# Normalize every sentence in the text
-sentences = sent_tokenize(text)
-stemmedSentences = []
-sentenceValue = dict()
-for sentence in sentences:
-    stemmedSentence = []
-    for word in sentence.lower().split():
-        stemmedSentence.append(stemmer.stem(word))
-    stemmedSentences.append(stemmedSentence)
-
-# Calculate value of every normalized sentence based on word frequency table
-# [:12] helps to save space
-for num in range(len(stemmedSentences)):
-    for wordValue in freqTable:
-        if wordValue in stemmedSentences[num]:
-            if sentences[num][:12] in sentenceValue:
-                sentenceValue[sentences[num][:12]] += freqTable.get(wordValue)
-            else:
-                sentenceValue[sentences[num][:12]] = freqTable.get(wordValue)
-
-# Determine average value of a sentence in the text
-sumValues = 0
-for sentence in sentenceValue:
-    sumValues += sentenceValue.get(sentence)
-
-average = int(sumValues / len(sentenceValue))
-
-# Create summary of text using sentences that exceed the average value by some factor
-# This factor can be adjusted to reduce/expand the length of the summary
-summary = ""
-for sentence in sentences:
-        if sentence[:12] in sentenceValue and sentenceValue[sentence[:12]] > (3.0 * average):
-            summary += " " + " ".join(sentence.split())
-
-# Process the text in summary and write it to a new file
-summary = re.sub("’", "'", summary)
-summary = re.sub("[^a-zA-Z0-9'\"():;,.!?— ]+", " ", summary)
-summaryText = open(fileName + "Summary.txt", "w")
-summaryText.write(summary)
-summaryText.close()
+if option == "text":
+    text = extractText(pdfFileName)
+    summarize(text)
+elif option == "OCR":
+    text = extractOCR(pdfFileName)
+    summarize(text)
+else:
+    print("Not a valid option!")
